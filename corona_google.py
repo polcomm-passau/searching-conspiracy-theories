708acfa82a2eee7d87636f7ab89ada00fb38339a
Daria (Dasha) Kravets <krawez@hotmail.com>
2022-04-18 21:21:55 +0200

    Update corona_google.py

--- corona_google.py
+++ corona_google.py
@@ -20,7 +20,6 @@ import random
 
 
 user_agent_list = [
-   #Chrome
    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36",
